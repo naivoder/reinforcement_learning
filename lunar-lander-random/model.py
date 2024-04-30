64f32b2f96659ccb62be5b1a0a5e1ff92026480e
credovian <cameron.redovian@r-dex.com>
2024-04-30 08:47:12 -0400

    policy network definition for LL env

---
+++ lunar-lander-random/model.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+class LanderNN(nn.Module):
+    def __init__(self, input_dims, n_actions, lr):
+        super(LanderNN, self).__init__()
+        self.device = "cuda" if torch.gpu.is_available() else "cpu"
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.lr = lr
+        self.model = nn.Sequential(
+            nn.Linear(*self.input_dims, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.n_actions),
+        ).to(self.device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+    def forward(self, x):
+        return self.model(x)
