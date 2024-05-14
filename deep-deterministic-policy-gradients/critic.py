6cb92e936a002d1055dcb67cbd9ddedf95f2cf98
credovian <cameron.redovian@r-dex.com>
2024-05-14 11:31:44 -0400

    working on critic network

---
+++ deep-deterministic-policy-gradients/critic.py
@@ -0,0 +1,70 @@
+import torch
+import numpy as np
+
+
+class CriticNetwork(torch.nn.Module):
+    def __init__(
+        self,
+        input_shape,
+        output_shape,
+        h1_size=400,
+        h2_size=300,
+        lr=1e-3,
+        decay=1e-2,
+        chkpt_path="model.pt",
+    ):
+        super(CriticNetwork, self).__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.h1_size = h1_size
+        self.h2_size = h2_size
+        self.lr = lr
+        self.decay = decay
+        self.chkpt_path = chkpt_path
+        self.h1_layer = torch.nn.Linear(*self.input_shape, self.h1_size)
+        self.h2_layer = torch.nn.Linear(self.h1_size, self.h2_size)
+        self.out_layer = torch.nn.Linear(self.h2_size, self.output_shape)
+        self.optimizer = torch.optim.Adam(lr=self.lr, weight_decay=self.decay)
+        self.init_weights()
+
+    def init_weights(self):
+        torch.nn.init.uniform_(
+            self.h1_layer.weight,
+            -1 / np.sqrt(*self.input_shape),
+            1 / np.sqrt(*self.input_shape),
+        )
+        torch.nn.init.uniform_(
+            self.h2_layer.weight,
+            -1 / np.sqrt(self.h1_size),
+            1 / np.sqrt(self.h1_size),
+        )
+        torch.nn.init.uniform_(
+            self.out_layer.weight,
+            -3e-3,
+            3e-3,
+        )
+
+    def forward(self, x):
+        x = torch.nn.ReLU(self.h1_layer(x))
+        x = torch.nn.ReLU(self.h2_layer(x))
+        x = torch.nn.BatchNorm1d(x)
+        return torch.nn.ReLU(self.out_layer(x))
+
+    def save_checkpoint(self, epoch, loss):
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": self.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+                "loss": loss,
+            },
+            self.chkpt_path,
+        )
+
+    def load_checkpoint(self, chkpt_path):
+        chkpt = torch.load(chkpt_path)
+        self.load_state_dict(chkpt["model_state_dict"])
+        self.optimizer.load_state_dict(chkpt["optimizer_state_dict"])
+        epoch = chkpt["epoch"]
+        loss = chkpt["loss"]
+        return epoch, loss
