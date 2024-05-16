9eecaadd63229ff6c325d3e3ff20ae76695713f8
credovian <cameron.redovian@r-dex.com>
2024-05-16 09:28:47 -0400

    add choose_action, store_transition, save/load interfaces

--- deep-deterministic-policy-gradients/agent.py
+++ deep-deterministic-policy-gradients/agent.py
@@ -13,13 +13,19 @@ class DDPGAgent(torch.nn.Module):
         self.action_noise = action_noise
 
     def choose_action(self, state):
-        pass
+        state = torch.Tensor(np.array(state)).to(self.actor.device)
+        actions = self.actor(state)
+        action_probs = torch.distributions.categorical(actions)
+        action = action_probs.sample()
+        return action.item()
 
     def store_transition(self, state, action, reward, next_state, done):
-        pass
+        self.replay_buffer.store_transition(state, action, reward, next_state, done)
 
-    def save_checkpoints(self):
-        pass
+    def save_checkpoints(self, epoch, loss):
+        self.actor.save_checkpoint(epoch, loss)
+        self.critic.save_checkpoint(epoch, loss)
 
     def load_checkpoints(self):
-        pass
+        self.actor.load_checkpoint()
+        self.critic.load_checkpoint()
