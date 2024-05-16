39e966eeefcc7a4e39513f49f8374a8b07e5eeb6
credovian <cameron.redovian@r-dex.com>
2024-05-16 09:47:38 -0400

    import other classes here instead of main, fix choose_action

--- deep-deterministic-policy-gradients/agent.py
+++ deep-deterministic-policy-gradients/agent.py
@@ -1,23 +1,49 @@
 import torch
 import numpy as np
+from noise import OrnsteinUhlenbeckActionNoise
+from actor import ActorNetwork
+from critic import CriticNetwork
+from memory import ReplayBuffer
 
 
 class DDPGAgent(torch.nn.Module):
-    def __init__(self, actor, critic, replay_buffer, action_noise):
+    def __init__(
+        self,
+        input_dims,
+        n_actions,
+        tau,
+        alpha=1e-4,
+        beta=1e-3,
+        gamma=0.99,
+        batch_size=64,
+        mem_size=1e6,
+    ):
         super(self, DDPGAgent).__init__()
-        self.actor = actor
-        self.critic = critic
-        self.target_actor = actor
-        self.target_critic = critic
-        self.replay_buffer = replay_buffer
-        self.action_noise = action_noise
+        self.tau = tau
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.batch_size = batch_size
+
+        self.replay_buffer = ReplayBuffer(mem_size, input_dims, n_actions)
+        self.action_noise = OrnsteinUhlenbeckActionNoise(np.zeros(input_dims))
+
+        self.actor = ActorNetwork(alpha, input_dims, n_actions)
+        self.target_actor = ActorNetwork(alpha, input_dims, n_actions)
+
+        self.critic = CriticNetwork(beta, input_dims, n_actions)
+        self.target_critic = CriticNetwork(beta, input_dims, n_actions)
+
+        self.update_network_parameters(tau=1)
 
     def choose_action(self, state):
-        state = torch.Tensor(np.array(state)).to(self.actor.device)
-        actions = self.actor(state)
-        action_probs = torch.distributions.categorical(actions)
-        action = action_probs.sample()
-        return action.item()
+        self.actor.eval()
+        state = torch.Tensor(np.array(state), dtype=torch.float).to(self.actor.device)
+        mu = self.actor(state).to(self.actor.device)
+        # add noise to deterministic output
+        mu = mu + torch.tensor(self.noise(), dtype=torch.float()).to(self.actor.device)
+        self.actor.train()
+        return mu.cpu().detach().numpy()[0]
 
     def store_transition(self, state, action, reward, next_state, done):
         self.replay_buffer.store_transition(state, action, reward, next_state, done)
