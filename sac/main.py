105e1feb762e64891eda81ee4c314721d17e49cc
credovian <cameron.redovian@r-dex.com>
2024-06-21 13:41:27 -0400

    fixing error with pybullet/gymnasium compatibility

---
+++ sac/main.py
@@ -0,0 +1,55 @@
+import gymnasium as gym
+import utils
+from agent import SACAgent
+import numpy as np
+from collections import UserDict
+import gym.envs.registration
+
+# Do this before importing pybullet_envs
+# (adds an extra property env_specs as a property to the registry,
+# so it looks like the <0.26 envspec version)
+registry = UserDict(gym.envs.registration.registry)
+registry.env_specs = gym.envs.registration.registry
+gym.envs.registration.registry = registry
+
+import pybullet_envs
+
+N_GAMES = 250
+
+env = gym.make("InvertedPendulumBulletEnv-v0")
+agent = SACAgent(
+    env.observation_space.shape,
+    env.action_space,
+    tau=5e-3,
+    reward_scale=2,
+    batch_size=256,
+)
+
+scores = []
+for i in range(N_GAMES):
+    state, _ = env.reset()
+
+    term, trunc, score = False, False, 0
+    while not term and not trunc:
+        action = agent.choose_action(state)
+        next_state, reward, term, trunc, _ = env.step(action)
+        score += reward
+
+        agent.store_transition(state, action, reward, next_state, term or trunc)
+        agent.learn()
+
+        state = next_state
+
+    scores.append(score)
+    avg_score = np.mean(scores[-100:])
+
+    if avg_score > best_score:
+        best_score = avg_score
+        agent.save_checkpoints()
+
+    print(
+        f"[Episode {i + 1:04}/{N_GAMES}]\tScore = {score:.4f}\tAverage = {avg_score:4f}",
+        end="\r",
+    )
+
+utils.plot_running_avg(scores)
