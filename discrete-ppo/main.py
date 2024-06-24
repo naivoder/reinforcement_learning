be946c4fe09b301f289f7c0233ffe780cc6b8c9b
credovian <cameron.redovian@r-dex.com>
2024-06-24 17:20:02 -0400

    added main

---
+++ discrete-ppo/main.py
@@ -0,0 +1,53 @@
+from agent import DiscretePPOAgent
+import numpy as np
+from utils import plot_running_avg
+import gymnasium as gym
+
+
+env = gym.make("Cartpole-v0")
+
+agent = DiscretePPOAgent(
+    env.observation_space.shape,
+    env.action_space.n,
+    alpha=3e-4,
+    epochs=10,
+    batch_size=64,
+)
+
+STEPS = 20
+N_GAMES = 300
+
+n_steps, n_learn = 0, 0
+scores, best_score = [], env.reward_range[0]
+for i in range(N_GAMES):
+    state, _ = env.reset()
+
+    term, trunc, score = False, False, 0
+    while not term and not trunc:
+        action, prob, val = agent.choose_action(state)
+
+        next_state, reward, term, trunc, _ = env.step(action)
+        score += reward
+
+        agent.memory.store_transition(state, prob, val, action, reward, term or trunc)
+
+        n_steps += 1
+        if n_steps % STEPS == 0:
+            agent.learn()
+            n_learn += 1
+
+        state = next_state
+
+    scores.append(score)
+    avg_score = np.mean(scores[-100:])
+
+    if avg_score > best_score:
+        best_score = avg_score
+        agent.save_checkpoints()
+
+    print(
+        f"[Episode {i + 1:04}/{N_GAMES}]\tScore = {score:.4f}\tAverage = {avg_score:4f}",
+        end="\r",
+    )
+
+plot_running_avg(scores)
